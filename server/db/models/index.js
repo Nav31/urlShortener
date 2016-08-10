6596ff632f6eda08fe4e6b1bc15659e07acda6a8
Nav31 <nsingh3104@gmail.com>
2016-08-10 11:41:17 -0400

    creates a randomly generated urlEnding and saves and checks the database to see if already existing

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,15 +1,10 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 
 const url  = new mongoose.Schema({ 
 	url : {type: String, required: true},
-	madeupUrl: {type: String, required: true},
+	urlEnd: {type: String, required: true},
 	date: { type: Date, default: Date.now }
-})
-
-url.pre('save', next => {
-	
-
-	next();
 })
 
 module.exports = mongoose.model('Url', url);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,28 @@
 const router = require('express').Router();
 const mongoose = require('mongoose');
 const Url = mongoose.model('Url');
+const crypto = require('crypto');
 module.exports = router;
 
 router.get('/:url', (req, res, next) => {
-	// /api/www.google.com
+	let buffr;
 	Url.findOne({url: req.params.url})
-	.then(url => { return url ? url : Url.create({url: url})})
+	.then(url => { 
+		if(url) return url;
+		else {
+			buffr = crypto.randomBytes(5).toString('hex');
+			return Url.find({})
+		}
+	})
+	.then(allUrls => {
+		if(!Array.isArray(allUrls)) return allUrls;
+		while(true){
+			let filteredUrls = allUrls.filter(ele => ele.urlEnd === buffr)
+			if(filteredUrls.length === 0) break
+			else buffr = crypto.randomBytes(5).toString('hex');
+		}
+		return Url.create({url: req.params.url, urlEnd: buffr})
+	})
 	.then(url => res.send(url))
 	.catch(console.log.bind(console))
 })
