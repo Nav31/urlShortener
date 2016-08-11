2f2457b1919439923f5c35bf8ee42537390114bd
Nav31 <nsingh3104@gmail.com>
2016-08-11 12:15:39 -0400

    creates working controller, factory, index.html

--- browser/app.js
+++ browser/app.js
@@ -1,1 +1,28 @@
+'use strict';
 
+var app = angular.module('urlShortener', []);
+
+app.controller('shortenerCtrl',function($scope, UrlFactory, $log){
+	$scope.shortUrl = null;
+
+	$scope.getShortUrl = function(){	
+		console.log('you clicked me')
+		UrlFactory.getShortUrl($scope.inputUrl)
+		.then(function(shortUrl){
+			$scope.shortUrl = shortUrl;
+		})
+		.catch($log);
+	}
+})
+
+
+app.factory('UrlFactory', function($http) {
+	function parseData (res) {
+		return res.data;
+	}
+	return {
+		getShortUrl: function(longUrl) {
+			return $http.get('api/' + longUrl).then(parseData);
+		}
+	}
+})
--- server/index.js
+++ server/index.js
@@ -5,13 +5,15 @@ const path = require('path');
 const startDb = require('./db');
 const routes = require('./routes');
 
-app.use(express.static(path.join(__dirname, 'node_modules')));
-app.use(express.static(path.join(__dirname, "browser")));
+app.use(express.static(path.join(__dirname, '/../node_modules')));
+app.use(express.static(path.join(__dirname, "/../browser")));
 
 app.use('/api', routes);
 
+app.get('/*', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
+
 const port = process.env.PORT || 1337;
 
-startDb.then(() => server.listen(port, () => console.log('Chillin on Port: ', port)))
+startDb.then(() => server.listen(port, () => console.log('Chillin on Port:', port)))
 	   .catch(error => console.error(error));
 
