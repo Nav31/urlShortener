dbab205ef4473573575a38feaa6f62adc9707839
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-08-16 12:24:54 -0400

    fixes url parsing

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,23 +6,37 @@ module.exports = router;
 
 router.post('/', (req, res, next) => {
 	let buffr;
-	Url.findOne({url: req.body.url})
+	let fixedUrl = parseUrl(req.body.url);
+	Url.findOne({ url: fixedUrl })
 	.then(url => { 
 		if(url) return url;
 		else {
 			buffr = crypto.randomBytes(5).toString('hex');
-			return Url.find({})
+			return Url.find({ urlEnd: buffr });
 		}
 	})
 	.then(allUrls => {
-		if(!Array.isArray(allUrls)) return allUrls;
-		while(true){
-			let filteredUrls = allUrls.filter(ele => ele.urlEnd === buffr)
-			if(filteredUrls.length === 0) break
-			else buffr = crypto.randomBytes(5).toString('hex');
+		if(!Array.isArray(allUrls)) return allUrls
+		else {
+			while(true){
+				let filteredUrls = allUrls.filter(ele => ele.urlEnd === buffr)
+				if(filteredUrls.length === 0) break
+				else buffr = crypto.randomBytes(5).toString('hex');
+			}
+			return Url.create({url: fixedUrl, urlEnd: buffr})
 		}
-		return Url.create({url: req.body.url, urlEnd: buffr})
 	})
 	.then(url => res.send(url))
 	.catch(console.log.bind(console))
 })
+
+
+function parseUrl(url) {
+	const httpStr = 'http://';
+	const www = 'www.';
+	const httpReg = /^https?:\/\/www\./;
+	const wwwReg = new RegExp(www);
+	if(!httpReg.test(url) && wwwReg.test(url)) url = httpStr + url;
+	else if(!httpReg.test(url) && !wwwReg.test(url)) url = httpStr + www + url;
+	return url;
+}
