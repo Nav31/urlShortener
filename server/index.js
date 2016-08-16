16604c39339d09c2d98973446d8b2c93cc5f9f5c
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-08-16 11:42:17 -0400

    adds favicon and reorder folders, creates also public folder for css and icon

--- server/index.js
+++ server/index.js
@@ -5,9 +5,12 @@ const path = require('path');
 const startDb = require('./db');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
 
 app.use(express.static(path.join(__dirname, '/../node_modules')));
 app.use(express.static(path.join(__dirname, "/../browser")));
+app.use(express.static(path.join(__dirname, "/../public")));
+app.use(favicon(path.join(__dirname, "/../public/favicon.ico")));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
