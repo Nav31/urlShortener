aec551753ed3aa812ad1cefade6892b0bd8fdce1
Nav31 <nsingh3104@gmail.com>
2016-08-16 14:40:56 -0400

    counter feature is now entirely complete

--- browser/app.js
+++ browser/app.js
@@ -8,6 +8,7 @@ app.controller('shortenerCtrl',function($scope, UrlFactory, $log){
 	$scope.getShortUrl = function(){	
 		UrlFactory.getShortUrl($scope.inputUrl)
 		.then(function(shortUrl){
+			$scope.clickCount = shortUrl.whenClicked.length;
 			$scope.shortUrl = 'localhost:1337/' + shortUrl.urlEnd;
 		})
 		.catch($log);
