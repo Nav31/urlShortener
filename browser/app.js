e3a9926bd446989db62bf13ebd26c49d09fdd2ca
Nav31 <nav31@users.noreply.github.com>
2016-09-09 12:25:36 -0400

    changed url length limit

--- browser/app.js
+++ browser/app.js
@@ -5,7 +5,7 @@ var app = angular.module('urlShortener', []);
 app.controller('shortenerCtrl',function($scope, UrlFactory, $log){
 	var clipboard = new Clipboard('.btn');
 	const testURl = url => {
-		let regex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+		let regex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,2000}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
 		return regex.test(url);
 	}
 	$scope.shortUrl = null;
